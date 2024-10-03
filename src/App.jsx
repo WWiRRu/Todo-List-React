fc9dbadee18263899beb464256309420ddfa414e
WWiRRu <92327076+WWiRRu@users.noreply.github.com>
2024-10-03 16:14:50 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import ListComponent from './components/ListComponent.jsx';
+function App() {
+  return (
+    <>
+        <ListComponent/>
+    </>
+  )
+}
+
+export default App
